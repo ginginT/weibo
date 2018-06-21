c913e05b28003a7f8e793207e9d2770a7dccd7cf
gingin <X_SJian@163.com>
2018-06-21 14:59:53 +0000

    Finish static_pages and layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img src="/public/imgs/weibo.jpg" class="brand-icon">
+            <a href="{{ route('home') }}">
+                随时随地发现新鲜
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+@section('title','微博')
+@section('content')
+<div class="jumbotron">
+    <h1>Hello Life</h1>
+    <p class="lead">
+        微博-随时随地发现新鲜！
+    </p>
+    <p>
+        一切，将从这里开始。
+    </p>
+    <p>
+        <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+    </p>
+</div>
+@stop
