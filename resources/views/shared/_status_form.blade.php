475ffdd7e4c9fc06e6829ad4ac52e8fc89e6098d
gingin <X_SJian@163.com>
2018-06-28 07:00:16 +0000

    Add user-followings

--- resources/views/shared/_status_form.blade.php
+++ resources/views/shared/_status_form.blade.php
@@ -1,6 +1,6 @@
 <form action="{{ route('statuses.store') }}" method="post">
     @include('shared._errors')
     {{ csrf_field() }}
-    <textarea name="content" class="form-control" rows="3" placeholder="聊聊新鲜事儿...">{{ old('content') }}</textarea>
-    <button type="submit" class="btn btn-primary pull-right">发布</button>
+    <textarea style="border: 1px solid #df65ff" name="content" class="form-control" rows="3" placeholder="聊聊新鲜事儿...">{{ old('content') }}</textarea>
+    <button style="background-color: #e961e9;" type="submit" class="btn btn-primary pull-right">发布</button>
 </form>
